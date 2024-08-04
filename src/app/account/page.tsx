03285333f8cef846681802b29f7ecb233fe2e1d0
GabrielMendonca1 <garimeipod@gmail.com>
2024-08-04 00:57:18 -0300

    ok

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,7 +3,7 @@ import NameForm from '@/components/ui/account/NameForm';
 import SignOut from '@/components/ui/auth-components/SignOut';
 import CustomerPortalForm from '@/components/ui/account/CustomerPortalForm';
 import Header from '@/components/ui/header/Header';
-import Pricing from '@/components/ui/pricing/Pricing';
+import Pricing from '@/components/ui/Pricing/Pricing';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/server';
 import {
